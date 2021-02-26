3fece90dea4439267b7b9b99b02fbffc38449255
Doshy <josh@jwhitehead.co.uk>
2021-02-26 23:22:30 +0000

    Menu sounds

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,8 +6,19 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
 
+    public AudioSource source;
+
     public void StartGame()
     {
+        StartCoroutine(StartGameCoroutine());
+    }
+
+    IEnumerator StartGameCoroutine()
+    {
+        source.Play();
+
+        yield return new WaitForSeconds(1);
+
         SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
     }
 
